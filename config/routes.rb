289240cb989d41937cee949cf1d09f02977ab045
kento.karakawa <kento.karakawa@gmail.com>
2015-11-16 03:17:09 +0900

    [modify] ルートの変更

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-    get 'ranking/history/:cate' => 'ranking#history'
+  get '/' => 'ranking#history'
+  get 'ranking/history/:cate' => 'ranking#history'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
